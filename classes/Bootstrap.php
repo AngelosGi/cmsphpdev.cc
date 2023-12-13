8ea0f65b71c6922eb96e6d314692971456340644
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-12-13 19:13:25 +0200

    fix internal error /htaccess errors

--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -8,12 +8,12 @@ class Bootstrap {
     public function __construct($request){
         $this->request = $request;
 
-        if($this->request['controller'] == ''){
+        if($this->request['controller'] == ""){
             $this->controller = 'home';
         } else {
             $this->controller = $this->request['controller'];
         }
-        if($this->request['action'] == ''){
+        if($this->request['action'] == ""){
             $this->action = 'index';
         } else {
             $this->action = $this->request['action'];
