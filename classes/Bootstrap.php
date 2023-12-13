6a040b5055e366490cfc96a6fd470ba399f19ee8
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-12-13 22:20:43 +0200

    fix(controller): add error handling for controller class and method

--- classes/Bootstrap.php
+++ classes/Bootstrap.php
@@ -18,6 +18,31 @@ class Bootstrap {
         } else {
             $this->action = $this->request['action'];
         }
-        echo $this->controller;
+        // echo $this->controller;
+        // echo $this->action;
     }
+
+    public function createController() {
+        //check class
+        if(class_exists($this->controller)){
+            $parents = class_parents($this->controller);
+            //check extend
+            if(in_array("Controller", $parents)){
+                if(method_exists($this->controller, $this->action)){
+                return new $this->controller($this->action, $this->request);
+                } else {
+                    //method does not exist
+                    echo 'Method does not exist';
+                    return;
+            } 
+            } else {
+            //base controller not found
+            echo 'Base controller not found';
+            return;
+        } 
+    } else {
+        echo "Controller class does not exist";
+        return;
+    }
+    } 
 }
