8aaaea33a89a798736f36070d3f315dfc8ef299c
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-12-14 21:46:57 +0200

    feat(core): add Model class and database configuration

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+//define db parameters
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "123456");
+define("DB_NAME", "shareboard");
+
+
+//define URL
+
+define("ROOT_PATH", "/");
+define('ROOT', 'http://localhost/php.dev');
+
