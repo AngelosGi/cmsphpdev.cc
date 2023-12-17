22c580834d06f3c06d797062e3190b8c693cb5c2
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-12-17 21:49:52 +0200

    chore(config): update database credentials and URLs

--- classes/Model.php
+++ classes/Model.php
@@ -7,4 +7,6 @@ abstract class Model {
     public function __construct(){
         $this->dbh = new PDO("mysql:host=" . DB_HOST . "dbname=" . DB_NAME , DB_USER, DB_PASS);
     }
+
+    
 }
--- config.php
+++ config.php
@@ -2,13 +2,13 @@
 
 //define db parameters
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
+define("DB_USER", "root1");
 define("DB_PASS", "123456");
-define("DB_NAME", "shareboard");
+define("DB_NAME", "postboarddb");
 
 
 //define URL
 
 define("ROOT_PATH", "/");
-define('ROOT', 'http://localhost/php.dev');
+define('ROOT_URL', 'http://localhost/cmsphpdev.cc');
 
