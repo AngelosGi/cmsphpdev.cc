9d2a21059a173b8d88e1db4225c63d74e390366c
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-12-18 18:36:51 +0200

    feat(Model): add query and bind methods
    This commit adds two new methods to the `Model` class: `query` and `bind`.
    
    The `query` method prepares a PDO statement based on a given SQL query. It sets `$this->stmt` to the prepared statement.
    
    The `bind` method allows for binding parameters to the prepared statement. It accepts three arguments: `$param` (the parameter name), `$value` (the value to be bound), and an optional `$type` argument that specifies the data type of the parameter. If `$type` is not provided, the method automatically determines the appropriate PDO constant based on the value's type.
    
    Both methods enhance the functionality of the `Model` class and enable better interaction with the underlying database.

--- classes/Model.php
+++ classes/Model.php
@@ -7,6 +7,27 @@ abstract class Model {
     public function __construct(){
         $this->dbh = new PDO("mysql:host=" . DB_HOST . "dbname=" . DB_NAME , DB_USER, DB_PASS);
     }
+    public function query($query){
+        $this->stmt = $this->dbh->prepare($query);
+    }
 
+    public function bind ($param, $value, $type = null){
+        if(is_null($type)){
+            switch(true){
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value);
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($type);
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default;
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->stmt->bindValue($param, $value, $type);
+    }
     
 }
