7370b6bbe1e625bfeb0ed31797db519dd0ee16aa
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-12-18 20:23:41 +0200

    fix(controller): fix typo in method name

--- controllers/home.php
+++ controllers/home.php
@@ -3,6 +3,6 @@
 class Home extends Controller {
     protected function Index(){
         $viewmodel = new HomeModel();
-        $this->returnView($viewmodel->Index(), true);
+        $this->ReturnView($viewmodel->Index(), true);
     }
 }
