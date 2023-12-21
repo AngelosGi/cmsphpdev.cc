62c35e513b5c66ffa29e346204c66c5c6dc1a265
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-12-21 18:48:03 +0200

    Add card layout for displaying posts

--- views/Posts/index.php
+++ views/Posts/index.php
@@ -1,1 +1,17 @@
-This is Post/index
+<div class="d-flex justify-content-center flex-column align-items-center">
+    <!-- <a class="btn btn-success btn-share" href="<?php echo ROOT_PATH; ?>shares/add">Share Something</a> -->
+    <?php foreach($viewmodel as $item) : ?>
+
+
+        <div class="card bg-light mb-3 d-flex justify-content-center" style="max-width: 70rem;">
+        <div class="card-header "><small><?php echo $item['create_date']; ?></small></div>
+        <div class="card-body">
+            <h5 class="card-title"><?php echo $item['title'] ?></h5>
+            <p class="card-text"><?php echo $item['body']; ?></p>
+            <a class="btn btn-default" href="<?php echo $item['link']; ?>" target="_blank">Get me there.</a>
+        </div>
+        </div>
+    <?php endforeach; ?>
+
+
+</div>
