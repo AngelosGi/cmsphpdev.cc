c080a459744dc82951f8b87c268d1ae236396676
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-12-21 17:07:09 +0200

    chore(views): update main.php navbar links

--- views/main.php
+++ views/main.php
@@ -12,25 +12,31 @@
 <!-- navbar-dark fixed-top bg-dark -->
 <nav class="navbar navbar-expand-md navbar-default">
   <div class="container-fluid">
-    <a class="navbar-brand" href="<?php echo ROOT_URL?>">Post Board</a>
+    <a class="navbar-brand" href="<?php echo ROOT_URL?>">cmsphpdev.cc</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+    
     <div class="collapse navbar-collapse" id="navbarCollapse">
+
       <ul class="navbar-nav me-auto mb-2 mb-md-0">
-        <!-- <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="<?php echo ROOT_URL?>">Home</a>
-        </li> -->
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="<?php echo ROOT_URL?>posts">Posts</a>
         </li>
-        <!-- <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li> -->
-        <!-- <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li> -->
       </ul>
+
+      <ul class="navbar-nav  mb-2 mb-md-0 navbar-right">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="<?php echo ROOT_URL?>users//login">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="<?php echo ROOT_URL?>users/register">Register</a>
+        </li>
+        
+      </ul>
+
+
+
       <!-- <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
