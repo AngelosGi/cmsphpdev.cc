d2cfb3d6d9ccd89e6f78405804a6c30e44dbaa4b
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-12-22 19:57:57 +0200

    Add lastInsertId method to Model class

--- classes/Model.php
+++ classes/Model.php
@@ -39,4 +39,10 @@ abstract class Model {
         $this->execute();
         return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function lastInsertId(){
+        return $this->dbh->lastInsertId();
+    }
+
+
 }
