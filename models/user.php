a9fd651db48b331b86aaf8c53d63e6b1ffb003be
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-12-27 19:51:54 +0200

    Add user registration functionality

--- models/user.php
+++ models/user.php
@@ -2,6 +2,26 @@
 
 class UserModel extends Model{
     public function register(){
+        $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        
+        $password = md5($post['password']);
+        
+        if($post && $post['submit'] && !empty($post['name'])){
+            // Insert into MySQL
+            $this->query('INSERT INTO users (name, email, password) VALUES(:name, :email, :password)');
+            $this->bind(':name', $post['name']);
+            $this->bind(':email', $post['email']);
+            $this->bind(':password', $password);
+            $this->execute();
+            // Verify
+            if($this->lastInsertId()){
+                //redirect
+                header('Location: '.ROOT_URL.'users/login');
+            }
+
+        }
         return;
+
+
     }
 }
