5b40c8cd5312f0b136f227f33b87f85ecbb6e4e2
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-12-28 16:26:10 +0200

    Refactor navigation links in main.php

--- views/main.php
+++ views/main.php
@@ -20,27 +20,20 @@
     <div class="collapse navbar-collapse" id="navbarCollapse">
 
       <ul class="navbar-nav me-auto mb-2 mb-md-0">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="<?php echo ROOT_URL?>posts">Posts</a>
-        </li>
+        <li class="nav-item"><a class="nav-link" aria-current="page" href="<?php echo ROOT_URL; ?>posts">Posts</a></li>
       </ul>
 
       <ul class="navbar-nav  mb-2 mb-md-0 navbar-right">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="<?php echo ROOT_URL?>users//login">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="<?php echo ROOT_URL?>users/register">Register</a>
-        </li>
-        
+        <?php if(isset($_SESSION['is_logged_in'])) : ?>
+        <li class="nav-item"><a class="nav-link" aria-current="page" href="<?php echo ROOT_URL; ?>">Welcome <?php echo $_SESSION['user_data']['name']; ?></a></li>
+        <li class="nav-item"><a class="nav-link" aria-current="page" href="<?php echo ROOT_URL; ?>users/logout">Logout</a></li>
+
+          <?php else : ?>
+        <li class="nav-item"><a class="nav-link" aria-current="page" href="<?php echo ROOT_URL; ?>users//login">Login</a></li>
+        <li class="nav-item"><a class="nav-link" aria-current="page" href="<?php echo ROOT_URL; ?>users/register">Register</a></li>
+          <?php endif; ?>
       </ul>
 
-
-
-      <!-- <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form> -->
     </div>
   </div>
 </nav>
@@ -48,7 +41,7 @@
 <div class="container">
 
 <div class="row">
-    <?php require($view);?>
+    <?php require($view); ?>
 </div>
 
 </div>
