188fe2634557cf0bac3e9e4e46e464dfd0674739
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-12-28 15:40:59 +0200

    Refactor user registration and login logic

--- models/user.php
+++ models/user.php
@@ -4,9 +4,11 @@ class UserModel extends Model{
     public function register(){
         $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         
-        $password = md5($post['password']);
         
         if($post && $post['submit'] && !empty($post['name'])){
+
+        $password = md5($post['password']);
+
             // Insert into MySQL
             $this->query('INSERT INTO users (name, email, password) VALUES(:name, :email, :password)');
             $this->bind(':name', $post['name']);
@@ -26,14 +28,15 @@ class UserModel extends Model{
     public function login(){
         $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         
-        $password = md5($post['password']);
-        
-        if($post['submit']){
+        if($post && isset($post['submit'], $post['email'], $post['password']) && !empty($post['email']) && !empty($post['password'])){
+            
+            $password = md5($post['password']);
             // compare
             $this->query('SELECT * FROM users WHERE email = :email AND password = :password');
             $this->bind(':email', $post['email']);
             $this->bind(':password', $password);
             $row = $this->single();
+            
             if($row){
                 echo 'Logged in';
             } else {
