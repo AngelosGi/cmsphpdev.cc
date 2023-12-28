fec1ef0f3c81108abd79bb6929095880642303ec
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-12-28 16:49:17 +0200

    Add session check for adding posts

--- controllers/posts.php
+++ controllers/posts.php
@@ -7,6 +7,9 @@ class Posts extends Controller {
     }
 
     protected function add(){
+        if(!isset($_SESSION['is_logged_in'])){
+            header('Location: '.ROOT_URL.'posts');
+        }
         $viewmodel = new PostModel();
         $this->ReturnView($viewmodel->add(), true);
     }
